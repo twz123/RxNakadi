b9bb82862e91b7fbb395573304934cb514851626
Tom Wieczorek <tom.wieczorek@zalando.de>
2016-12-19 16:28:44 +0100

    Use Rx response dispatching also for error case of streamed responses

--- src/main/java/org/zalando/rxnakadi/http/DelimitedJsonStreamTarget.java
+++ src/main/java/org/zalando/rxnakadi/http/DelimitedJsonStreamTarget.java
@@ -4,21 +4,21 @@ import static java.nio.charset.StandardCharsets.ISO_8859_1;
 
 import static java.util.Objects.requireNonNull;
 
+import static org.zalando.rxnakadi.http.AhcResponseDispatch.responseString;
+import static org.zalando.rxnakadi.http.AhcResponseDispatch.statusCode;
 import static org.zalando.rxnakadi.http.NakadiHttpClient.X_NAKADI_STREAM_ID;
+import static org.zalando.rxnakadi.rx.dispatch.RxDispatch.dispatch;
+import static org.zalando.rxnakadi.rx.dispatch.RxDispatch.on;
 
-import static com.google.common.net.HttpHeaders.CONTENT_LENGTH;
 import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
 import java.nio.charset.Charset;
 
-import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.regex.Pattern;
 
 import org.asynchttpclient.HttpResponseBodyPart;
+import org.asynchttpclient.HttpResponseHeaders;
 import org.asynchttpclient.HttpResponseStatus;
 
 import org.reactivestreams.Publisher;
@@ -33,7 +33,6 @@ import com.google.common.net.MediaType;
 
 import io.netty.handler.codec.http.HttpHeaders;
 
-import rx.Observable;
 import rx.RxReactiveStreams;
 import rx.Subscriber;
 
@@ -63,23 +62,30 @@ final class DelimitedJsonStreamTarget extends SubscriptionTarget<String> {
     public boolean startBodyStream(final HttpResponseStatus responseStatus, final HttpHeaders responseHeaders,
             final Publisher<HttpResponseBodyPart> publisher) {
 
-        LOG.debug("Response: [{} {}], headers: [{}]", responseStatus.getStatusCode(), responseStatus.getStatusText(),
+        final int statusCode = responseStatus.getStatusCode();
+        LOG.debug("Response: [{} {}], headers: [{}]", statusCode, responseStatus.getStatusText(),
             responseHeaders.entries());
 
         if (subscriber.isUnsubscribed()) {
             return false;
         }
 
-        if (responseStatus.getStatusCode() == 200) {
+        if (statusCode == 200) {
             ok(responseHeaders, publisher);
             return true;
         }
 
-        if (responseStatus.getStatusCode() == 409) {
-            return conflict(responseStatus, responseHeaders, publisher);
-        }
-
-        throw new UnsupportedOperationException("Unsupported status code: " + responseStatus.getStatusCode());
+        RxReactiveStreams.toObservable(publisher)                                                //
+                         .lift(StreamedResponses.takeBytes(4096))                                //
+                         .compose(StreamedResponses.toResponse(responseStatus,
+                                 new HttpResponseHeaders(responseHeaders)))                      //
+                         .toSingle()                                                             //
+                         .flatMap(dispatch(statusCode(),                                         //
+                                 on(409).error(response ->
+                                         new IllegalStateException(responseString(response)))))  //
+                         .toCompletable()                                                        //
+                         .subscribe(subscriber);
+        return true;
     }
 
     private void ok(final HttpHeaders responseHeaders, final Publisher<HttpResponseBodyPart> publisher) {
@@ -104,63 +110,11 @@ final class DelimitedJsonStreamTarget extends SubscriptionTarget<String> {
             nakadiStreamId.accept(responseHeaders.get(X_NAKADI_STREAM_ID));
         }
 
-        final Charset charset = charset(contentType);
+        final Charset charset = contentType.charset().or(ISO_8859_1);
         RxReactiveStreams.toObservable(publisher)                          // reactive streams -> RxJava 1.x
                          .map(HttpResponseBodyPart::getBodyPartBytes)      // parts -> bytes
                          .compose(bytes -> Strings.decode(bytes, charset)) // bytes -> chars
                          .compose(Transformers.split(delimiterPattern))    // chars -> chunks
                          .subscribe(subscriber);
-    }
-
-    private boolean conflict(final HttpResponseStatus responseStatus, final HttpHeaders responseHeaders,
-            final Publisher<HttpResponseBodyPart> publisher) {
-
-        Observable
-            .just(responseHeaders.get(CONTENT_TYPE))                                                                   //
-            .filter(Objects::nonNull)                                                                                  //
-            .map(MediaType::parse)                                                                                     //
-            .filter(contentType -> contentType.is(MediaType.create("application", "problem+json")))                    //
-            .zipWith(contentLength(responseHeaders),
-                (contentType, contentLength) -> toResponseString(charset(contentType), contentLength, publisher))      //
-            .flatMap(observable -> observable)                                                                         //
-            .onErrorResumeNext(error ->
-                    Observable.error(new IllegalStateException("Conflict: " + responseStatus.getUri(), error)))        //
-            .flatMap(response ->
-                    Observable.error(
-                        new IllegalStateException("Conflict: " + responseStatus.getUri() + ": " + response)))          //
-            .switchIfEmpty(Observable.error(new IllegalStateException("Conflict: " + responseStatus.getUri())))
-            .toCompletable()                                                                                           //
-            .subscribe(subscriber);
-
-        return !subscriber.isUnsubscribed();
-    }
-
-    private static Observable<Long> contentLength(final HttpHeaders responseHeaders) {
-        return Observable.just(responseHeaders.get(CONTENT_LENGTH)) //
-                         .map(contentLength -> contentLength == null ? -1 : Long.valueOf(contentLength));
-    }
-
-    private static Observable<String> toResponseString(final Charset charset, final long contentLength,
-            final Publisher<HttpResponseBodyPart> publisher) {
-
-        if (contentLength > 32 * 1024) {
-            return Observable.error(new UnsupportedOperationException("Response body too large: " + contentLength));
-        }
-
-        return RxReactiveStreams.toObservable(publisher) //
-                                .collect(() ->
-                                        new ByteArrayOutputStream(contentLength < 0 ? 512 : (int) contentLength),
-                                    (bytes, part) -> {
-                                        try {
-                                            bytes.write(part.getBodyPartBytes());
-                                        } catch (final IOException e) {
-                                            throw new RuntimeException(e.getMessage(), e);
-                                        }
-                                    })                   //
-                                .map(bytes -> new String(bytes.toByteArray(), charset));
-    }
-
-    private static Charset charset(final MediaType contentType) {
-        return contentType.charset().or(ISO_8859_1);
     }
 }
